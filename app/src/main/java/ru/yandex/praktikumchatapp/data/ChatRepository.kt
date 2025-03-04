afbd6a73faa9f96df70ce109054781780498b0fd
Aleksey <mrLiocha@yandex.ru>
2025-03-05 02:15:25 +0300

    sprint 2

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,26 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
 
+    val DELAY_FACTOR = 2
+    var currentDelay = 100L
+
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()
+        val flow = api.getReply()
+        return flow.retryWhen { cause, _ ->
+            if (cause is Exception) {
+                delay(currentDelay)
+                currentDelay *= DELAY_FACTOR
+                true
+            } else {
+                false
+            }
+        }
     }
 }
